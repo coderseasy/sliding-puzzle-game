aa13fabeb8388c34349cb2c777df6161c0b063c2
coderseasy <ebertescalant@gmail.com>
2022-08-04 16:00:04 -0500

    add draw_stuff func

--- main.cpp
+++ main.cpp
@@ -8,7 +8,30 @@
 
 using namespace std;
 
-//<<<<<<< HEAD
+void draw_stuff(int index, double tx, double ty){
+	char string[8][256];
+	
+	unsigned int count;
+	void* stroke_fonts[2]={
+		GLUT_STROKE_ROMAN,
+		GLUT_STROKE_ROMAN
+	};
+	for(int i=0;i<8;i++){
+		count=0;
+		string[i][count]=49+i;
+		string[i][++count]='\0';
+	}
+	glColor3f(0.0,0.0,0.0);
+	int stroke_scale=10;
+	glPushMatrix();
+	{
+		glTranslatef(tx,ty,0);
+		glScalef(0.2,0.2,0.2);
+		print_stroke_string(GLUT_STROKE_ROMAN,string[index]);
+	}
+	glPopMatrix();
+}
+
 void drawGrid(){
 	int i;
 	glColor3f(1,0,0);
@@ -39,9 +62,7 @@ void display(){
     
 
 }
-//=======
 
-//>>>>>>> Ebert
 int main(int argc, char** argv){
 		//cout<<" PROYECTO "<<endl;
 
