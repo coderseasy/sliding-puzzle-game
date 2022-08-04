cbcef88bc5050b59f2a59a4f4d2d58e48f7c903a
coderseasy <ebertescalant@gmail.com>
2022-08-04 14:06:20 -0500

    comm

--- main.cpp
+++ main.cpp
@@ -8,21 +8,6 @@
 
 using namespace std;
 
-void drawGrid(){
-	int i;
-	glColor3f(1,0,0);
-	glBegin(GL_LINES);
-	{
-		for(i=-60;i<=60;i=i+40){
-			glVertex2f(-60,i);
-			glVertex2f(60,i);
-			
-			glVertex2f(i,-60);
-			glVertex2f(i,60);
-		}
-	}
-	glEnd();
-}
 
 void display(){
 	glClearColor(0,0,0,0);
