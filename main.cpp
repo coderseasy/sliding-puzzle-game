6e79648b99e7e942fd27bf2a70e2ebd50c9645d2
chana-samy <181242@unamba.edu.pe>
2022-08-04 13:50:31 -0500

    commit1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
-
+#include <bits/stdc++.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
 #include <iostream>
+#include <math.h>
+
+#include <windows.h>
 
 using namespace std;
 
+void display(){
+	glClearColor(0,0,0,0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-70, 70, -70, 70, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glColor3f(0,1,0);
+	glutSwapBuffers();
+}
+
 int main(int argc, char** argv){
-	
+		//cout<<" PROYECTO "<<endl;
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow(" PROYECTO !");
+	glutDisplayFunc(display);
+	glutMainLoop();
 	
     
     return 0;
