897036ecca3ea2904c019498748ef2f8ac2ca6bd
coderseasy <ebertescalant@gmail.com>
2022-08-04 14:04:59 -0500

    add drawGrid func

--- main.cpp
+++ main.cpp
@@ -8,6 +8,22 @@
 
 using namespace std;
 
+void drawGrid(){
+	int i;
+	glColor3f(1,0,0);
+	glBegin(GL_LINES);
+	{
+		for(i=-60;i<=60;i=i+40){
+			glVertex2f(-60,i);
+			glVertex2f(60,i);
+			
+			glVertex2f(i,-60);
+			glVertex2f(i,60);
+		}
+	}
+	glEnd();
+}
+
 void display(){
 	glClearColor(0,0,0,0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
